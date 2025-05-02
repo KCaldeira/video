628b95c6fa2aa9358bf4d205c5920217d158733a
KCaldeira <kcaldeira@carnegiescience.edu>
2025-05-01 21:39:49 -0700

    bug fixes

--- post_process.py
+++ post_process.py
@@ -49,7 +49,7 @@ def triangular_filter_odd(data, N):
     return filtered
 
 
-def post_process(csv, prefix, vars, metrics, process_list, ticks_per_frame, cc_number, filter_width):
+def post_process(csv, prefix, vars, metrics, process_list, ticks_per_beat, beats_per_minute, frames_per_second, cc_number, filter_narrow, filter_wide):
 
     if not os.path.exists(f"../video_midi/{prefix}"):
         os.makedirs(f"../video_midi/{prefix}")
@@ -61,22 +61,21 @@ def post_process(csv, prefix, vars, metrics, process_list, ticks_per_frame, cc_n
             if key not in csv.columns:
                 continue
             process_dict = {}
-            # first apply a narrow triangular filter
-            process_dict[var + "_" + metric + "_p" ] = triangular_filter_odd(csv[key], filter_width)
 
             if "rank" in process_list:
+                process_dict[key + "_v"] = csv[key]
+                process_dict[key + "_r"] = percentile_data(csv[key])            # first apply a narrow triangular filter
+            
+            
+                        # apply an additional 25 imot wode troamgular filter upon request
+            if "filter" in process_list:
                 process_dict_copy = process_dict.copy()
                 for key in process_dict_copy:
-                    normalized_metric = percentile_data(process_dict_copy[key])
-                    process_dict[key + "_r"] = normalized_metric
-
-            # apply an additional 25 imot wode troamgular filter upon request
-            if "f25" in process_list:
-                process_dict_copy = process_dict.copy()
-                for key in process_dict_copy:
-                    process_dict[key+"_f25"] = triangular_filter_odd(process_dict_copy[key], 25)
+                    process_dict[key + "_f" + str(filter_narrow)] = triangular_filter_odd(process_dict_copy[key], filter_narrow)
+                    process_dict[key + "_f" + str(filter_wide)] = triangular_filter_odd(process_dict_copy[key], filter_wide)
 
             # scale the data to 0-1
+            process_dict_copy = process_dict.copy()
             for key in process_dict_copy:
                 process_dict[key] = scale_data(process_dict[key])
 
@@ -95,57 +94,62 @@ def post_process(csv, prefix, vars, metrics, process_list, ticks_per_frame, cc_n
 
             # now write out everything for this var and metric as a single midi file
 
-
+        ticks_per_frame = ticks_per_beat * beats_per_minute / (60 *frames_per_second)
         frame_count_list = csv.index.tolist()
-        for key in process_dict:
-            midi_file = mido.MidiFile()
-            midi_val_base = [round(104 * val) for val in process_dict[key]]
-            track_base = mido.MidiTrack()
-            track_base.append(mido.MetaMessage('track_name', name=key, time=0))
-            for i, midi_value in enumerate(midi_val_base):
-                time_tick = 0 if i == 0 else int(ticks_per_frame * (frame_count_list[i] - frame_count_list[i - 1]))
-                track_base.append(
-                    mido.Message('control_change',
-                            control=cc_number,
-                            value=midi_value,
-                            channel=0,
-                            time=time_tick))
-            midi_file.tracks.append(track_base)
-                    
-        midi_file.save("../video_midi/" + prefix + "/" + var + "_" + metric + ".mid")
+        for var in vars:
+            for metric in metrics:
+                for suffix in ["v", "r"]:
+                    key = var + "_" + metric  +  "_" + suffix # The base metric is the "_pos" version
+                    if key not in master_dict:
+                        continue
+                    midi_file = mido.MidiFile()
+                    for full_key in master_dict:
+                        if key not in full_key:
+                            continue
+                        midi_track = mido.MidiTrack()
+                        midi_file.tracks.append(midi_track)
+
+                        midi_track.append(mido.MetaMessage('track_name', name=full_key, time=0))
+
+                        midi_val_base = [round(104 * val) for val in master_dict[full_key]]
+
+                        for i, midi_value in enumerate(midi_val_base):
+                            time_tick = 0 if i == 0 else int(ticks_per_frame * (frame_count_list[i] - frame_count_list[i - 1]))
+                            midi_track.append(
+                                mido.Message('control_change',
+                                        control=cc_number,
+                                        value=midi_value,
+                                        channel=0,
+                                        time=time_tick))
+
+                        
+                    midi_file.save("../video_midi/" + prefix + "/" + key + ".mid")
 
     # write out the master xlsx
+    master_dict['frame_count_list'] = frame_count_list
     master_df = pd.DataFrame(master_dict)
+    # Reorder columns to put frame_count_list first
+    cols = ['frame_count_list'] + [col for col in master_df.columns if col != 'frame_count_list']
+    master_df = master_df[cols]
     master_df.to_excel(prefix + "_derived.xlsx", index=False)
-
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print(f"Derived data saved to {prefix}_derived.xlsx")
 
 
 
 if __name__ == "__main__":
     prefix = "MzUL2-5jm3f"
-    csv = pd.read_csv(prefix + ".csv", index_col=0)
+    csv = pd.read_csv(prefix + "_basic.csv", index_col=0)
 
     vars= ["R", "G", "B","Gray","HSV"]
-    metric_names = ["avg", "var", "lrg", "xps", "rfl", "rad", "lin","ee1","ee2","ee3","ed1","ed2","ed3","es1","es2","es3"]
-    process_list = ["neg","rank", "power","inv","f25"]
-    ticks_per_frame = 480
+    metric_names = ["avg", "var", "lrg", "xps", "rfl", "rad", "lmd","l10","l90","ee1","ee2","ee3","ed1","ed2","ed3","es1","es2","es3"]
+    process_list = ["neg","rank", "power","inv","filter"]
+    ticks_per_beat = 480
+    beats_per_minute=92
+    frames_per_second=30
     cc_number = 1
-    filter_width = 5
+    filter_narrow = 5
+    filter_wide = 25
 
-    post_process(csv, prefix, vars, metric_names, process_list, ticks_per_frame, cc_number, filter_width)
+    post_process(csv, prefix, vars, metric_names, process_list, ticks_per_beat, beats_per_minute, frames_per_second, cc_number, filter_narrow, filter_wide)
 
 
